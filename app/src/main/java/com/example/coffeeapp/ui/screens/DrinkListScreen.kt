1b37d0221bd22f515e8fd845d858a72b71866364
LEQUANHOANGNAM <lenam.160304@gmail.com>
2025-04-21 22:17:33 +0700

    Update Menu Screen clickabel

--- app/src/main/java/com/example/coffeeapp/ui/screens/DrinkListScreen.kt
+++ app/src/main/java/com/example/coffeeapp/ui/screens/DrinkListScreen.kt
@@ -1,7 +1,9 @@
 package com.example.coffeeapp.ui.screens
 
+import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,8 +13,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -26,26 +26,27 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.example.coffeeapp.model.DrinkData
+import com.example.coffeeapp.ui.SetStatusBarIconsLight
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import coil.compose.AsyncImage
-import com.example.coffeeapp.ui.SetStatusBarIconsLight
+import com.google.gson.Gson
 
 @Composable
-fun DrinkListScreen(categoryId: String) {
+fun DrinkListScreen(categoryId: String, navController: NavController) {
 
     SetStatusBarIconsLight(isLightIcons = true)
-    //API
     val drinks = remember { mutableStateListOf<DrinkData>() }
 
     LaunchedEffect(categoryId) {
         Log.d("DEBUG", "Loading drinks for categoryId: $categoryId")
         val database = FirebaseDatabase.getInstance(
             "https://coffeeappshoputh-default-rtdb.asia-southeast1.firebasedatabase.app"
-        ).reference //get api
+        ).reference
 
         database.child("Items")
             .addListenerForSingleValueEvent(object : ValueEventListener {
@@ -79,42 +80,51 @@ fun DrinkListScreen(categoryId: String) {
                 .background(color = Color.Black)
         ) {
             items(drinks) { drink ->
-                Button(
-                    onClick = {},
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.LightGray,
-                        contentColor = Color.White ),
-                    shape = RoundedCornerShape(16.dp),
+                Row(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(8.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(Color.DarkGray)
+                        .clickable {
+                            val drinkJson = Uri.encode(Gson().toJson(drink))
+                            navController.navigate("drink_detail/$drinkJson")
+                        },
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Row(
+                    AsyncImage(
+                        model = drink.picUrl.firstOrNull() ?: "",
+                        contentDescription = "drink_image",
                         modifier = Modifier
-                            .fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically
+                            .clip(RoundedCornerShape(16.dp))
+                            .size(100.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(start = 5.dp)
+                            .weight(1f)
                     ) {
-                        AsyncImage(
-                            model = drink.picUrl.firstOrNull() ?: "",
-                            contentDescription = "drink_image",
-                            modifier = Modifier
-                                .clip(RoundedCornerShape(16.dp))
-                                .size(100.dp),
-                            contentScale = ContentScale.Crop
+                        Text(
+                            text = drink.title,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 15.sp,
+                            color = Color.White,
+                            modifier = Modifier.clickable {
+                                val drinkJson = Uri.encode(Gson().toJson(drink))
+                                navController.navigate("drink_detail/$drinkJson")
+                            }
                         )
-                        Column(
-                            modifier = Modifier
-                                .padding(start = 5.dp)
-                                .weight(1f)
-                        ) {
-                            Text(text = drink.title, fontWeight = FontWeight.Bold, fontSize = 15.sp)
-                            Text(text = drink.description, fontSize = 12.sp)
-                            Text(text = "Price: ${String.format("%.2f", drink.price)}$", fontWeight = FontWeight.Bold, fontSize = 15.sp)
-                        }
+                        Text(text = drink.description, fontSize = 12.sp, color = Color.LightGray)
+                        Text(
+                            text = "Price: ${String.format("%.2f", drink.price)}$",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 15.sp,
+                            color = Color.White
+                        )
                     }
                 }
             }
         }
     }
 }
-
